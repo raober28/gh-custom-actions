177d7fe39f6ce480dc4978c93b0ee8eb5b17906f
roberoi <v-roberoi@expediagroup.com>
2025-05-21 18:22:26 +0530

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -13,7 +13,10 @@ function run() {
     const s3Uri = `s3://${bucket}`
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion} `);
 
-    core.notice('Hello from my custom javascript action')
+    const websiteUrl = `https://${bucket}.s3.website-${bucketRegion}.amazonaws.com`
+    core.setOutput('website-url', websiteUrl) // ::set-output
+
+
 }
 
 run();
